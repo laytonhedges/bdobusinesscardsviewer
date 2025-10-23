b7fa82ab0133ed583d0cec9cceea588cee47d83b
Layton Hedges <LHedges@bdo.co.za>
2025-10-23 09:10:39 +0200

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'card',
+    component: App,
+  },
+  {
+    path: '**',
+    redirectTo: 'card',
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+
+interface BusinessCardDetails {
+  fullName?: string;
+  jobTitle?: string;
+  department?: string;
+  email?: string;
+  phone?: string;
+  extraDetails?: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('BdoBusinessCards');
+  public businessCardDetails?: BusinessCardDetails;
+
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((query) => {
+      const businessCardDetails: BusinessCardDetails = {};
+
+      businessCardDetails.fullName = query['fullName'] ?? 'Joe Soap';
+      businessCardDetails.jobTitle = query['jobTitle'] ?? 'Senior Auditor';
+      businessCardDetails.department = query['department'] ?? 'AUDIT';
+      businessCardDetails.email = query['email'] ?? 'jsoap@bdo.co.za';
+      businessCardDetails.phone = query['phone'] ?? '(011) 112-3456';
+      businessCardDetails.extraDetails = query['extraDetails'] ?? null;
+
+      this.businessCardDetails = businessCardDetails;
+    });
+  }
 }
